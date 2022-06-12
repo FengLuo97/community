1ff3da24357382bb81554e09c399d021bbe945c3
FengLuo97 <3167881811@qq.com>
2022-06-12 13:23:50 +0800

    【MOD】首页以及分页查询帖子功能实现

--- src/test/java/com/fengluo/community/MapperTests.java
+++ src/test/java/com/fengluo/community/MapperTests.java
@@ -1,10 +1,8 @@
 package com.fengluo.community;
 
 import com.fengluo.community.dao.DiscussPostMapper;
-import com.fengluo.community.dao.LoginTicketMapper;
 import com.fengluo.community.dao.UserMapper;
 import com.fengluo.community.entity.DiscussPost;
-import com.fengluo.community.entity.LoginTicket;
 import com.fengluo.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,8 +22,6 @@ public class MapperTests {
     @Autowired
     private UserMapper userMapper;
 
-    @Autowired
-    private LoginTicketMapper loginTicketMapper;
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
@@ -81,24 +77,4 @@ public class MapperTests {
         System.out.println(rows);
     }
 
-    @Test
-    public void testInsertLoginTicket() {
-        LoginTicket loginTicket = new LoginTicket();
-        loginTicket.setUserId(101);
-        loginTicket.setTicket("abc");
-        loginTicket.setStatus(0);
-        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
-
-        loginTicketMapper.insertLoginTicket(loginTicket);
-    }
-
-    @Test
-    public void testSelectLoginTicket() {
-        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
-        System.out.println(loginTicket);
-
-        loginTicketMapper.updateStatus("abc", 1);
-        loginTicket = loginTicketMapper.selectByTicket("abc");
-        System.out.println(loginTicket);
-    }
 }
